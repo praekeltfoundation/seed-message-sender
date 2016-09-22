3621a811750c9116731f2c15aa192186692a70aa
Rudi Giesler <rudi@praekelt.com>
2016-09-22 15:53:48 +0200

    Add address formatter for vas2nets sms line

--- message_sender/formatters.py
+++ message_sender/formatters.py
@@ -13,3 +13,14 @@ def vas2nets_voice(msisdn):
     MSISDNs prefixed with a 9 instead of the country code to initiate an OBD.
     """
     return re.sub(r'\+?234(\d+)$', r'90\1', msisdn)
+
+
+def vas2nets_text(msisdn):
+    """
+    FIXME: this should not need be in this repo
+
+    Vas2Nets is an aggregator in Nigeria, they need MSISDNs in the local
+    format, prefixed with a 0, instead of the international format with the
+    country code.
+    """
+    return re.sub(r'\+?234(\d+)$', r'0\1', msisdn)
