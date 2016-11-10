142861b291a3145d74176abb7410a41cec0e7f3b
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-11-10 17:25:59 +0200

    Adjusted migration

--- message_sender/models.py
+++ message_sender/models.py
@@ -25,7 +25,7 @@ class Outbound(models.Model):
     metadata = JSONField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    last_sent_time = models.DateTimeField(null=True)
+    last_sent_time = models.DateTimeField(null=True, blank=True)
     created_by = models.ForeignKey(User, related_name='outbounds_created',
                                    null=True)
     updated_by = models.ForeignKey(User, related_name='outbounds_updated',
