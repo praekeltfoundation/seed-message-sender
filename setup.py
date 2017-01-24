8f65cd7225eaf1762115ff68a12eada98de74088
Simon de Haan <simon@praekelt.org>
2017-01-24 15:31:40 +0100

    use latest seed-papertrail

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'go_http==0.3.0',
         'drfdocs==0.0.11',
         'django-redis-cache==1.7.1',
-        'seed-papertrail>=1.4.3',
+        'seed-papertrail>=1.5.1',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
