0531fd3d21679d4074d19c9820a357507e8290e2
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-02-15 17:49:37 +0200

    Undo all changes that don't affect the models

--- message_sender/tasks.py
+++ message_sender/tasks.py
@@ -219,11 +219,12 @@ class SendMessage(Task):
                         int(getattr(settings, 'VOICE_MESSAGE_TIMEOUT', 0)),
                         int(getattr(settings, 'VOICE_MESSAGE_DELAY', 0)))
                     sender = self.get_voice_client()
-                    # Start call. We send the voice message on the ack.
+                    speech_url = message.metadata["voice_speech_url"]
                     vumiresponse = sender.send_voice(
                         voice_to_addr_formatter(message.to_addr),
-                        None, session_event="new")
-                    message.call_answered = False
+                        message.content,
+                        speech_url=speech_url,
+                        session_event="new")
                     l.info("Sent voice message to <%s>" % message.to_addr)
                 else:
                     # Plain content
